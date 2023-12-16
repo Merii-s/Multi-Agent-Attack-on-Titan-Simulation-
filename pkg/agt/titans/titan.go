279b65f434317e4c1d2277b3ca2f135c71327eba
Merii-s <massil.bouzar1@gmail.com>
2023-12-16 19:30:43 +0100

    code clarity

--- pkg/agt/titans/titan.go
+++ pkg/agt/titans/titan.go
@@ -17,7 +17,10 @@ type Titan struct {
 }
 
 func NewTitan(id pkg.Id, t pkg.Type, p pkg.Position, hp int, r int, s int, spd int, h int, regen int) *Titan {
-	return &Titan{agentAttributes: *pkg.NewAgent(id, t, p, hp, r, s, spd), height: h, regenRate: regen}
+	return &Titan{
+		agentAttributes: *pkg.NewAgent(id, t, p, hp, r, s, spd),
+		height:          h,
+		regenRate:       regen}
 }
 
 func (t *Titan) RegenRate() int {
