10d3409a1cca7d5c2f84b20993cb001572817a52
Saad Lakramti <saad.lakramti@gmail.com>
2023-12-21 01:59:06 +0400

    UI change in structure

--- pkg/env.go
+++ pkg/env.go
@@ -1,45 +1,83 @@
 package pkg
 
-func NewEnvironement(H int, W int) *Environment {
-	//env := &pkg.Environment{objects:new(pkg.Objects), agents:new(pkg.Agents), screenH:H, screenW:W}
-
+func createStaticObjects(H int, W int) []Object {
 	objects := make([]Object, 0)
+	var obj *Object
 
 	//Grass Sprites
 	nW := W / CGrass
 	nH := H / CGrass
 	for i := 0; i < nH; i++ {
 		for j := 0; j < nW; j++ {
-			obj := NewObject(Grass, Position{X: j * CGrass, Y: i * CGrass}, 1000000000)
+			obj = NewObject(Grass, Position{X: j * CGrass, Y: i * CGrass}, 1000000000)
 			objects = append(objects, *obj)
 		}
 	}
 
 	//Wall Sprites
-
 	wall_Tl_X := int(0.2 * float32(W))
 	wall_Tl_Y := int(0.2 * float32(H))
-
-	wWall := int(0.6 * float32(W))
-	hWall := int((6. / 7.) * float32(H))
-
-	nW = wWall / CWall
-	nH = hWall / CWall
-
+	wWall := int(0.6 * float32(W))       //Largeur du mur
+	hWall := int((6. / 7.) * float32(H)) //Hauteur du mur
+	nW = wWall / CWall                   //Nombre de sprites en largeur
+	nH = hWall / CWall                   //Nombre de sprites en hauteur
 	//Mur du Nord, Horizontal
 	for i := 0; i < nW+1; i++ {
-		obj := NewObject(Wall, Position{X: wall_Tl_X + i*CWall, Y: wall_Tl_Y}, 1000000000)
+		obj = NewObject(Wall, Position{X: wall_Tl_X + i*CWall, Y: wall_Tl_Y}, 1000000000)
+		objects = append(objects, *obj)
+	}
+	//Murs cotes, Verticaux
+	for i := 1; i < nH; i++ {
+		obj = NewObject(Wall, Position{X: wall_Tl_X, Y: wall_Tl_Y + i*CWall}, 1000000000)
+		objects = append(objects, *obj)
+		obj = NewObject(Wall, Position{X: wall_Tl_X + wWall, Y: wall_Tl_Y + i*CWall}, 1000000000)
 		objects = append(objects, *obj)
 	}
 
-	//Murs cotes, Verticaux
-	for i := 1; i < nH; i++ {
-		obj1 := NewObject(Wall, Position{X: wall_Tl_X, Y: wall_Tl_Y + i*CWall}, 1000000000)
-		objects = append(objects, *obj1)
-		obj2 := NewObject(Wall, Position{X: wall_Tl_X + wWall, Y: wall_Tl_Y + i*CWall}, 1000000000)
-		objects = append(objects, *obj2)
+	//Champs
+	nW = ((wWall - 2*CWall) / 2) / WField //Nombre de sprites en largeur
+	for i := 0; i < nW*2; i++ {
+		var x, y int
+		if i < 7 {
+			x = wall_Tl_X + CWall + int(wWall/4) + i*WField
+			y = wall_Tl_Y + CWall + int(hWall/10)
+		} else {
+			x = wall_Tl_X + CWall + int(wWall/4) + (i-7)*WField
+			y = wall_Tl_Y + CWall + int(hWall/10) + HField*2
+		}
+		obj = NewObject(Field, Position{X: x, Y: y}, 1000000000)
+		objects = append(objects, *obj)
 	}
 
+	//Petites maisons
+	coefsCoords := [][]float32{{0.29, 0.4}, {1 - 0.29, 0.4}, {0.29, 0.85}, {1 - 0.29, 0.85}, {0.29, 0.55}, {1 - 0.29, 0.65}, {0.5, 0.85}}
+	for _, coords := range coefsCoords {
+		obj = NewObject(SmallHouse, Position{X: int(coords[0] * float32(W)), Y: int(coords[1] * float32(H))}, 1000000000)
+		objects = append(objects, *obj)
+	}
+
+	//Grandes maisons 1 et 2
+	coefsCoords = [][]float32{{0.29, 0.7}, {0.5, 0.55}, {0.4, 0.55}, {0.62, 0.7}}
+	for i, coords := range coefsCoords {
+		if i < 2 {
+			obj = NewObject(BigHouse1, Position{X: int(coords[0] * float32(W)), Y: int(coords[1] * float32(H))}, 1000000000)
+		} else {
+			obj = NewObject(BigHouse2, Position{X: int(coords[0] * float32(W)), Y: int(coords[1] * float32(H))}, 1000000000)
+		}
+		objects = append(objects, *obj)
+	}
+
+	//Donjons
+	obj = NewObject(Dungeon, Position{X: int(0.2*float32(W) + CWall), Y: int(0.2*float32(H) + CWall)}, 1000000000)
+	objects = append(objects, *obj)
+	obj = NewObject(Dungeon, Position{X: int(0.8*float32(W) - CWall - WDungeon/2), Y: int(0.2*float32(H) + CWall)}, 1000000000)
+	objects = append(objects, *obj)
+
+	return objects
+}
+
+func NewEnvironement(H int, W int) *Environment {
+	objects := createStaticObjects(H, W)
 	return &Environment{objects: objects}
 }
 
--- pkg/gui/gui.go
+++ pkg/gui/gui.go
@@ -6,12 +6,6 @@ import (
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
-	//types "AOT_local/AOT/pkg"
-)
-
-const (
-	Height = 700
-	Width  = 1000
 )
 
 type Game struct {
@@ -28,12 +22,7 @@ var (
 	dungeonImg *ebiten.Image
 	cannonImg  *ebiten.Image
 
-	opWall    ebiten.DrawImageOptions
-	opGrass   ebiten.DrawImageOptions
-	opField   ebiten.DrawImageOptions
-	opHouse   ebiten.DrawImageOptions
-	opDungeon ebiten.DrawImageOptions
-	op        ebiten.DrawImageOptions
+	op ebiten.DrawImageOptions
 
 	e *pkg.Environment
 )
@@ -70,6 +59,7 @@ func init() {
 	} else if err8 != nil {
 		log.Fatal(err8)
 	}
+
 	e = pkg.NewEnvironement(700, 1000)
 }
 
@@ -84,19 +74,19 @@ func drawSprite(screen *ebiten.Image, o pkg.Object) {
 
 	switch o.Name() {
 	case pkg.Field:
-		img, _, err = ebitenutil.NewImageFromFile(pkg.GetPath_Win("wheat_V2"))
+		img = fieldImg
 	case pkg.BigHouse1:
-		img, _, err = ebitenutil.NewImageFromFile(pkg.GetPath_Win("big_house_sprite"))
+		img = bHouse1Img
 	case pkg.BigHouse2:
-		img, _, err = ebitenutil.NewImageFromFile(pkg.GetPath_Win("big_house_spriteV2"))
+		img = bHouse2Img
 	case pkg.Dungeon:
-		img, _, err = ebitenutil.NewImageFromFile(pkg.GetPath_Win("dungeon_sprite"))
+		img = dungeonImg
 	case pkg.Grass:
-		img, _, err = ebitenutil.NewImageFromFile(pkg.GetPath_Win("grass_spriteV4"))
+		img = grassImg
 	case pkg.Wall:
-		img, _, err = ebitenutil.NewImageFromFile(pkg.GetPath_Win("wall_sprite"))
+		img = wallImg
 	default:
-		img, _, err = ebitenutil.NewImageFromFile(pkg.GetPath_Win("small_house_sprite"))
+		img = sHouseImg
 	}
 
 	if err != nil {
@@ -112,172 +102,20 @@ func drawEnvironment(screen *ebiten.Image, env *pkg.Environment) {
 	}
 }
 
-// dir : horizontal et sprite bien dimensionné
-func drawWall(Xs int, Ys int, Xe int, Ye int, dir bool, sprite *ebiten.Image, screen *ebiten.Image) {
-	imageBounds := sprite.Bounds()
-	w := imageBounds.Dx()
-	h := imageBounds.Dy()
-	if dir {
-		nbSprite := (Xe - Xs) / w
-		for i := 0; i < nbSprite; i++ {
-			opWall.GeoM.Reset()
-			opWall.GeoM.Translate(float64(Xs+i*w), float64(Ys))
-			screen.DrawImage(sprite, &opWall)
-		}
-	} else {
-		nbSprite := (Ye - Ys) / h
-		for i := 0; i < nbSprite; i++ {
-			opWall.GeoM.Reset()
-			opWall.GeoM.Translate(float64(Xs), float64(Ys+i*h))
-			screen.DrawImage(sprite, &opWall)
-		}
-	}
-}
-
-func drawDungeons(screen *ebiten.Image, dungeonImg *ebiten.Image, cWall int) {
-	imageBounds := dungeonImg.Bounds()
-	w := imageBounds.Dx()
-	opDungeon.GeoM.Reset()
-	opDungeon.GeoM.Translate(float64(0.2*Width+cWall), float64(0.2*Height+cWall))
-	screen.DrawImage(dungeonImg, &opDungeon)
-	opDungeon.GeoM.Reset()
-	opDungeon.GeoM.Translate(float64(0.8*Width-cWall-w/2), float64(0.2*Height+cWall))
-	screen.DrawImage(dungeonImg, &opDungeon)
-}
-
-func drawCannons(screen *ebiten.Image, cannonImg *ebiten.Image, cWall int) {
-	imageBounds := cannonImg.Bounds()
-	w := imageBounds.Dx()
-	//h := imageBounds.Dy()
-
-	opDungeon.GeoM.Reset()
-	opDungeon.GeoM.Translate(float64(0.2*Width+cWall), float64(0.2*Height+cWall))
-	screen.DrawImage(dungeonImg, &opDungeon)
-	opDungeon.GeoM.Reset()
-	opDungeon.GeoM.Translate(float64(0.8*Width-cWall-w/2), float64(0.2*Height+cWall))
-	screen.DrawImage(dungeonImg, &opDungeon)
-}
-
-func drawSmallHouses(screen *ebiten.Image, sHouseImg *ebiten.Image) {
-	coefsCoords := [][]float32{{0.29, 0.4}, {1 - 0.29, 0.4}, {0.29, 0.85}, {1 - 0.29, 0.85}, {0.29, 0.55}, {1 - 0.29, 0.65}, {0.5, 0.85}}
-	for _, coords := range coefsCoords {
-		opHouse.GeoM.Reset()
-		opHouse.GeoM.Translate(float64(coords[0]*Width), float64(coords[1]*Height))
-		screen.DrawImage(sHouseImg, &opHouse)
-	}
-}
-
-func drawBigHouses(screen *ebiten.Image, bHouse1Img *ebiten.Image, bHouse2Img *ebiten.Image) {
-	coefsCoords := [][]float32{{0.29, 0.7}, {0.5, 0.55}, {0.4, 0.55}, {0.62, 0.7}}
-	for i, coords := range coefsCoords {
-		opHouse.GeoM.Reset()
-		opHouse.GeoM.Translate(float64(coords[0]*Width), float64(coords[1]*Height))
-		if i < 2 {
-			screen.DrawImage(bHouse1Img, &opHouse)
-		} else {
-			screen.DrawImage(bHouse2Img, &opHouse)
-		}
-	}
-}
-
-func drawHouses(screen *ebiten.Image, sHouseImg *ebiten.Image, bHouse1Img *ebiten.Image, bHouse2Img *ebiten.Image) {
-	drawSmallHouses(screen, sHouseImg)
-	drawBigHouses(screen, bHouse1Img, bHouse2Img)
-}
-
-// Visuel dans lequel la ville est un rectangle au centre de la screen
-func drawCityRectWalls(screen *ebiten.Image, wallSprite *ebiten.Image, fieldSprite *ebiten.Image) {
-	// dimensions des sprites
-	wallBounds := wallSprite.Bounds()
-	fieldBounds := fieldSprite.Bounds()
-	cSpriteWall := wallBounds.Dx()
-	cSpriteField := fieldBounds.Dx()
-
-	xTL := 0.2 * Width
-	yTL := 0.2 * Height
-	xBR := 0.8 * Width
-	yBR := 0.8 * Height
-
-	// ------- Draw walls -------
-	// mur haut horizontal G --> D
-	drawWall(int(xTL), int(yTL), int(xBR)+cSpriteWall, int(yTL), true, wallSprite, screen)
-	// mur gauche vertical H --> B
-	drawWall(int(xTL), int(yTL+float64(cSpriteWall)), int(xTL), int(yBR), false, wallSprite, screen)
-	// mur bas horizontal G --> D
-	drawWall(int(xTL), int(yBR), int(xBR)+cSpriteWall, int(yBR), true, wallSprite, screen)
-	// mur droit vertical H --> B
-	drawWall(int(xBR), int(yTL+float64(cSpriteWall)), int(xBR), int(yBR), false, wallSprite, screen)
-
-	// ------- Draw fields -------
-	// field haut horizontal 1
-	drawWall(int(xTL)+cSpriteWall+0.2*1000, int(yTL)+cSpriteWall+0.18*Height, int(xBR)-cSpriteWall-0.2*Width, int(yTL)+cSpriteWall+0.18*Height, true, fieldSprite, screen)
-	drawWall(int(xTL)+cSpriteWall+0.2*1000, int(yTL)+cSpriteWall+0.18*Height+cSpriteField, int(xBR)-cSpriteWall-0.2*Width, int(yTL)-cSpriteWall+0.18*Height+cSpriteField, true, fieldSprite, screen)
-}
-
-// Visuel des murs de la ville comme nous l'avait decrit Massil
-func drawCityBorderWalls(screen *ebiten.Image, wallSprite *ebiten.Image, fieldSprite *ebiten.Image) {
-	// dimensions des sprites
-	wallBounds := wallSprite.Bounds()
-	fieldBounds := fieldSprite.Bounds()
-	cSpriteWall := wallBounds.Dx()
-	cSpriteField := fieldBounds.Dx()
-
-	xTL := 0.2 * Width
-	yTL := 0.2 * Height
-	xBR := 0.8 * Width
-	yBR := Height
-
-	// ------- Draw walls -------
-	// mur haut horizontal G --> D
-	drawWall(int(xTL), int(yTL), int(xBR)+cSpriteWall, int(yTL), true, wallSprite, screen)
-	// mur gauche vertical H --> B
-	drawWall(int(xTL), int(yTL+float64(cSpriteWall)), int(xTL), int(yBR), false, wallSprite, screen)
-	// mur droit vertical H --> B
-	drawWall(int(xBR), int(yTL+float64(cSpriteWall)), int(xBR), int(yBR), false, wallSprite, screen)
-
-	// ------- Draw fields -------
-	// field haut horizontal 1
-	drawWall(int(xTL)+cSpriteWall+0.2*1000, int(yTL)+cSpriteWall+0.18*Height, int(xBR)-cSpriteWall-0.2*Width, int(yTL)+cSpriteWall+0.18*Height, true, fieldSprite, screen)
-	drawWall(int(xTL)+cSpriteWall+0.2*1000, int(yTL)+cSpriteWall+0.18*Height+4*cSpriteField, int(xBR)-cSpriteWall-0.2*Width, int(yTL)-cSpriteWall+0.18*Height+4*cSpriteField, true, fieldSprite, screen)
-
-	drawHouses(screen, sHouseImg, bHouse1Img, bHouse2Img)
-	drawDungeons(screen, dungeonImg, cSpriteWall)
-
-}
-
-func drawGrass(screen *ebiten.Image, grassImg *ebiten.Image) {
-	bounds := grassImg.Bounds()
-	cSpriteGrass := bounds.Dx()
-
-	nbSpritesWidth := Width / cSpriteGrass
-	nbSpritesHeight := Height / cSpriteGrass
-
-	for i := 0; i < nbSpritesHeight; i++ {
-		for j := 0; j < nbSpritesWidth; j++ {
-			opGrass.GeoM.Reset()
-			opGrass.GeoM.Translate(float64(j*cSpriteGrass), float64(i*cSpriteGrass))
-			screen.DrawImage(grassImg, &opGrass)
-		}
-	}
-}
-
 func (g *Game) Update() error {
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	//drawGrass(screen, grassImg)
-	//drawCityBorderWalls(screen, wallImg, fieldImg)
-
 	drawEnvironment(screen, e)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return Width, Height
+	return 1000, 700
 }
 
 func RunDisplay() {
-	ebiten.SetWindowSize(Width, Height)
+	ebiten.SetWindowSize(1000, 700)
 	ebiten.SetWindowTitle("AOT Simulation")
 	if err := ebiten.RunGame(&Game{}); err != nil {
 		log.Fatal(err)
